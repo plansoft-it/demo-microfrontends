86574867a5a26a4c659659f84a8cdb044f5ea73c
iacopo ciao <iacopociao1992@gmail.com>
2019-11-24 16:18:47 +0100

    removed deprecated function

--- src/single-spa-examples.js
+++ src/single-spa-examples.js
@@ -1,19 +1,19 @@
-import {declareChildApplication, start} from 'single-spa';
+import {registerApplication, start} from 'single-spa';
 import {loadEmberApp} from 'single-spa-ember';
 import 'babel-polyfill';
 
-declareChildApplication('navbar', () => import('./navbar/navbar.app.js'), () => true);
-declareChildApplication('home', () => import('./home/home.app.js'), () => location.pathname === "" || location.pathname === "/");
-declareChildApplication('angularjs', () => import('./angularjs/angularjs.app.js'), pathPrefix('/angularjs'));
-declareChildApplication('workshops', () => import('./workshops/workshops.app.js'), pathPrefix('/workshops'));
-declareChildApplication('angular', () => import('./angular/angular.app.js'), pathPrefix('/angular'));
-declareChildApplication('vue', () => import('src/vue/vue.app.js'), pathPrefix('/vue'));
-declareChildApplication('svelte', () => import('src/svelte/svelte.app.js'), pathPrefix('/svelte'));
-declareChildApplication('preact', () => import('src/preact/preact.app.js'), pathPrefix('/preact'));
-declareChildApplication('iframe-vanilla-js', () => import('src/vanillajs/vanilla.app.js'), pathPrefix('/vanilla'));
-declareChildApplication('inferno', () => import('src/inferno/inferno.app.js'), pathPrefix('/inferno'));
-declareChildApplication('cyclejs', () => import('src/cyclejs/cycle.app.js'), pathPrefix('/cycle'));
-declareChildApplication('ember', () => loadEmberApp("ember-app", '/build/ember-app/assets/ember-app.js', '/build/ember-app/assets/vendor.js'), pathPrefix('/ember'));
+registerApplication('navbar', () => import('./navbar/navbar.app.js'), () => true);
+registerApplication('home', () => import('./home/home.app.js'), () => location.pathname === "" || location.pathname === "/");
+registerApplication('angularjs', () => import('./angularjs/angularjs.app.js'), pathPrefix('/angularjs'));
+registerApplication('workshops', () => import('./workshops/workshops.app.js'), pathPrefix('/workshops'));
+registerApplication('angular', () => import('./angular/angular.app.js'), pathPrefix('/angular'));
+registerApplication('vue', () => import('src/vue/vue.app.js'), pathPrefix('/vue'));
+registerApplication('svelte', () => import('src/svelte/svelte.app.js'), pathPrefix('/svelte'));
+registerApplication('preact', () => import('src/preact/preact.app.js'), pathPrefix('/preact'));
+registerApplication('iframe-vanilla-js', () => import('src/vanillajs/vanilla.app.js'), pathPrefix('/vanilla'));
+registerApplication('inferno', () => import('src/inferno/inferno.app.js'), pathPrefix('/inferno'));
+registerApplication('cyclejs', () => import('src/cyclejs/cycle.app.js'), pathPrefix('/cycle'));
+registerApplication('ember', () => loadEmberApp("ember-app", '/build/ember-app/assets/ember-app.js', '/build/ember-app/assets/vendor.js'), pathPrefix('/ember'));
 
 start();
 
