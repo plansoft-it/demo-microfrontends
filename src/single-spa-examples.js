7056e0a223f20e92d8585735c634268b7f77f985
iacopo ciao <iacopociao1992@gmail.com>
2019-12-01 16:52:15 +0100

    renames

--- src/single-spa-examples.js
+++ src/single-spa-examples.js
@@ -1,10 +1,9 @@
 import {registerApplication, start} from 'single-spa';
-import {loadEmberApp} from 'single-spa-ember';
 import 'babel-polyfill';
 
 registerApplication('navbar', () => import('./navbar/navbar.app.js'), () => true);
 registerApplication('home', () => import('./home/home.app.js'), () => location.pathname === "" || location.pathname === "/");
-registerApplication('speakers', () => import('./speakers/angularjs.app.js'), pathPrefix('/speakers'));
+registerApplication('speakers', () => import('./speakers/speakers.app.js'), pathPrefix('/speakers'));
 registerApplication('workshops', () => import('./workshops/workshops.app.js'), pathPrefix('/workshops'));
 registerApplication('frameworks', () => import('./frameworks/framework.app.js'), pathPrefix('/frameworks'));
 registerApplication('iframe-vanilla-js', () => import('src/vanillajs/vanilla.app.js'), pathPrefix('/vanilla'));
